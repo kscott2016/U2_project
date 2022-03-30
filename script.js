b79a4b74b68f430d60cdca397c536c5fb04d5a4d
Krystina <krystinascott2008@yahoo.com>
2022-03-30 03:01:56 -0500

    pull data of character input and styling

--- script.js
+++ script.js
@@ -13,10 +13,33 @@ button.addEventListener('click', async () => {
     //let response = await axios.get('https://spapi.dev/api/families').then((res) => { axios.get(res.data.data[0].characters[0]).then(res2 => { console.log(res2.data.data.name) }) })
     
     //console.log(response.data)
-    let matchQuery = "https://amiiboapi.com/api/amiibo?character="+ characterInput.value
+    let matchQueryName = "https://amiiboapi.com/api/amiibo?character="+ characterInput.value
 
-    let matches = await axios.get(matchQuery)
+    let matches = await axios.get(matchQueryName)
     console.log(matches);
+
+   // if (matches.data.amiibo.length === 1 ){
+        //if there's just one amiibo for the selected character, do this
+        let charName = matches.data.amiibo[0].character;
+        let refImg = matches.data.amiibo[0].image;
+        let gameSeries = matches.data.amiibo[0].gameSeries;
+        let amiiboSeries = matches.data.amiibo[0].amiiboSeries;
+        
+        
+        let amiiboType = matches.data.amiibo[0].type;
+
+        //set html elements
+        document.querySelector('h2').innerHTML = charName;
+        document.getElementById("character-image").src = `${refImg}`
+        
+        document.querySelector(".amiibo-series").innerHTML= `<span class="descriptor">Amiibo Series:</span> ${amiiboSeries}`;
+        document.querySelector(".game-series").innerHTML= `<span class="descriptor">Game Series:</span> ${gameSeries}`;
+        document.querySelector(".amiibo-type").innerHTML= `<span class="descriptor">Amiibo Type:</span> ${amiiboType}`;
+
+        
+   // }
+
+
 
     //let pic = response.data.url
     //let dogPic =  response.data.message
