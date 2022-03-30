8c5184b48e0e255a7df51190ab83adbd74047041
Krystina <krystinascott2008@yahoo.com>
2022-03-30 17:02:31 -0500

    clean styling

--- aboutscript.js
+++ aboutscript.js
@@ -4,21 +4,19 @@ let factChoices = document.querySelector('li')
 factChoices.addEventListener('click', async () => {
     console.log("CLICKED")
 
-    //if correct
+
     if(factChoices.classList.contains('correct')){
 
         factChoices.innerHTML="CORRECT"
         factChoices.classList.add('correct-fact')
     }  
 
-    //not correct
     else{
 
         factChoices.innerHTML="INCORRECT"
         factChoices.classList.add('wrong-fact')
 
     }
-
 })
 
 
